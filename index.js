adca43c6fdd46411f675dedecc1722007cb9f170
nitish17855 <nitishtripathi547@gmail.com>
2026-01-31 23:03:22 +0530

    Fix CORS for Vercel frontend

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.use(cors({
     "http://localhost:5173",
     "https://quizzo-frontend-26ru.vercel.app"
   ],
-  credentials: true
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.options("*", cors());
 
